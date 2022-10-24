a6d821323cf7ba0b802503d4c164890308adf6e3
BDacy <490352246@qq.com>
2022-10-24 23:19:01 +0800

    simple try 2

--- Bloom_Filter/src/BloomFilter.java
+++ Bloom_Filter/src/BloomFilter.java
@@ -12,8 +12,15 @@ public class BloomFilter {
     private static final int Default_BitSize = 100000;
 
     // 哈希函数种子的提供数组，数组的大小就是哈希函数的数量
-    private static final int[] seeds = new int[]{3,5,7,11,13,17,19};
+    private static final int[] Default_seeds = new int[]{3,5,7,11,13,17,19};
 
     // 使用BitSet数据结构来当作过滤器的位图
-    private static BitSet bitSet = new BitSet(Default_BitSize);
+    private static BitSet bitSet;
+
+    // 哈希函数组
+    private final HashFunction[] hashFunctions;
+
+    public BloomFilter(HashFunction[] hashFunctions) {
+        this.hashFunctions = hashFunctions;
+    }
 }
