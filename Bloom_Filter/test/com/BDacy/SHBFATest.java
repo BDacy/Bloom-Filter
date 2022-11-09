0a0947c935aea77de472975a3ffa0da33f001766
BDacy <490352246@qq.com>
2022-11-09 20:17:42 +0800

    编写对SHBFA.java 的测试

--- Bloom_Filter/test/com/BDacy/SHBFATest.java
+++ Bloom_Filter/test/com/BDacy/SHBFATest.java
@@ -5,10 +5,12 @@ import com.BDacy.A_Shifting_BloomFilter.SHBFA;
 import com.BDacy.A_Shifting_BloomFilter.SHBFDefaultConfig;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+import static com.BDacy.A_Shifting_BloomFilter.ElementBelong.*;
 import static org.junit.Assert.*;
 
 /**
@@ -53,16 +55,16 @@ public class SHBFATest {
         for (int i = 1; i <= 5; i++) set1.add(String.valueOf(i));
         for (int i = 4; i <= 8; i++) set2.add(String.valueOf(i));
         SHBFA<String> bf = new SHBFA<>(set1,set2,10000,5);
-        assertEquals(ElementBelong.S1_diff_S2,bf.query("1"));
-        assertEquals(ElementBelong.S1_diff_S2,bf.query("2"));
-        assertEquals(ElementBelong.S1_diff_S2,bf.query("3"));
+        assertEquals(S1_diff_S2,bf.query("1"));
+        assertEquals(S1_diff_S2,bf.query("2"));
+        assertEquals(S1_diff_S2,bf.query("3"));
 
-        assertEquals(ElementBelong.S1_and_S2,bf.query("4"));
-        assertEquals(ElementBelong.S1_and_S2,bf.query("5"));
+        assertEquals(S1_and_S2,bf.query("4"));
+        assertEquals(S1_and_S2,bf.query("5"));
 
-        assertEquals(ElementBelong.S2_diff_S1,bf.query("6"));
-        assertEquals(ElementBelong.S2_diff_S1,bf.query("7"));
-        assertEquals(ElementBelong.S2_diff_S1,bf.query("8"));
+        assertEquals(S2_diff_S1,bf.query("6"));
+        assertEquals(S2_diff_S1,bf.query("7"));
+        assertEquals(S2_diff_S1,bf.query("8"));
     }
 
     @Test
@@ -75,9 +77,38 @@ public class SHBFATest {
         SHBFA<String> bf = new SHBFA<>(set1,set2,10000,5);
 
         for (int i = 0; i < 500; i++) {
-            assertEquals(ElementBelong.Not_S1_or_S2,bf.query(UUID.randomUUID().toString()));
+            assertEquals(Not_S1_or_S2,bf.query(UUID.randomUUID().toString()));
         }
     }
+
+    @Test
+    public void SHBFAQueryBDTest()throws Exception{
+        System.out.println("SHBFA 枚举数据测试query的正确率");
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < 1000000; i++) set1.add(i);
+        for (int i = 800000; i < 1800000; i++) set2.add(i);
+        SHBFA<Integer> bf = new SHBFA<>(set1, set2, 2 << 24, 9);
+
+        int[] cnt = new int[9];
+        for (int i = 0; i < 2000000; i++)
+            cnt[checkBelong(bf.query(i))]++;
+
+        System.out.println(Arrays.toString(cnt));
+    }
+    public int checkBelong(ElementBelong eb){
+        return switch (eb) {
+            case S1_diff_S2 -> 1;
+            case S1_and_S2 -> 2;
+            case S2_diff_S1 -> 3;
+            case S1_UnSureS2 -> 4;
+            case UnSureS1_S2 -> 5;
+            case S1_diff_S2_or_S2_diff_S1 -> 6;
+            case S1_or_S2 -> 7;
+            case Not_S1_or_S2 -> 8;
+        };
+    }
+
 
     @Test
     public void SHBFAGetAppearProbabilityMethodTest()throws Exception{
@@ -89,16 +120,16 @@ public class SHBFATest {
         for (int i = 0; i < 400; i++) set1.add(UUID.randomUUID().toString());
         SHBFA<String> bf = new SHBFA<>(set1,set2,10000,5);
 
-        System.out.println(bf.getAppearProbability(ElementBelong.S1_diff_S2));
-        System.out.println(bf.getAppearProbability(ElementBelong.S1_and_S2));
-        System.out.println(bf.getAppearProbability(ElementBelong.S2_diff_S1));
+        System.out.println(bf.getAppearProbability(S1_diff_S2));
+        System.out.println(bf.getAppearProbability(S1_and_S2));
+        System.out.println(bf.getAppearProbability(S2_diff_S1));
 
-        System.out.println(bf.getAppearProbability(ElementBelong.UnSureS1_S2));
-        System.out.println(bf.getAppearProbability(ElementBelong.S1_diff_S2_or_S2_diff_S1));
-        System.out.println(bf.getAppearProbability(ElementBelong.S1_UnSureS2));
+        System.out.println(bf.getAppearProbability(UnSureS1_S2));
+        System.out.println(bf.getAppearProbability(S1_diff_S2_or_S2_diff_S1));
+        System.out.println(bf.getAppearProbability(S1_UnSureS2));
 
-        System.out.println(bf.getAppearProbability(ElementBelong.S1_or_S2));
-        System.out.println(bf.getAppearProbability(ElementBelong.Not_S1_or_S2));
+        System.out.println(bf.getAppearProbability(S1_or_S2));
+        System.out.println(bf.getAppearProbability(Not_S1_or_S2));
     }
 
     @Test
